4539898e93d7ac229a8177f3071bb0344c21fae2
victorcdrdm <victorcrombez@gmail.com>
2021-06-14 12:11:22 +0200

    rectification of texte

--- js/parallax.js
+++ js/parallax.js
@@ -6,28 +6,35 @@ let wedding = document.querySelector('.wedding')
 let cards = document.querySelectorAll('.block-card')
 let picture = document.querySelector('.picture-couple')
 let elementY = 0
-let i = 0;
-
-for (i = 0; i < cards.length; i++) {
-    elementY = cards[i].offsetTop
-    console.log(cards[i].children[0])
-    let index = cards[i].getAttribute('data-show')
-    action(elementY, index)
-}
-
-function action(elementY ,index){
-    window.addEventListener('scroll', () => {
-        const screenY = window.scrollY
-        if ((screenY-50/elementY) < 0.90 || screenY+50 <= elementY) {
-            for (i = 0; i < cards.length; i++) {
-                if (cards[i].getAttribute('data-show') === index ){
-                    cards[i].children[0].style.transform ='translateX('+((screenY-elementY)*0.065)+'px) translateY('+((screenY-elementY)*-0.09)+'px)';
-                    cards[i].children[1].style.transform ='translateY('+((screenY-elementY)*-0.18)+'px)';
-                }
-            }
-        }
-    })
-}
+let elementX = 0
+//
+// let i = 0;
+//
+// for (i = 0; i < cards.length ; i++) {
+//     elementY = cards[i].offsetTop
+//     let index = cards[i].getAttribute('data-show')
+//     action(elementY, index)
+// }
+//
+// function action(elementY ,index){
+//     window.addEventListener('scroll', () => {
+//         const screenY = window.scrollY
+//         const windowHeight = window.innerHeight * 0.90
+//         const  activeY = screenY + windowHeight
+//         if (activeY/elementY > 0.85 && activeY/elementY < 1.12) {
+//             for (i = 0; i < cards.length; i++) {
+//                 if (cards[i].getAttribute('data-show') === index ){
+//                     const element1 = (activeY-elementY)/12
+//                         cards[i].children[0].style.transform ='translateX('+ element1 + 'px)';
+//
+//
+//                     cards[i].children[1].style.transform ='translateY('+((screenY-elementY)*-0.099)+'px)';
+//                     console.log(window.innerWidth/2, cards[i].children[0].offsetWidth/2 + (activeY-elementY)/3.5)
+//                 }
+//             }
+//         }
+//     })
+// }
 let boxHeight = picture.offsetHeight * 0.3;
 const height  = window.innerHeight
 
@@ -60,7 +67,7 @@ function copy(int) {
     let copyText = document.querySelectorAll('.address')
     let selector = document.querySelectorAll('.annexe-copy-link')
     for (i=0 ; i<copyText.length; i++){
-       if (copyText[i].getAttribute('data-ads') == int) {
+       if (copyText[i].getAttribute('data-ads') === int) {
            console.log(copyText[i])
            const r = document.createRange();
            let c = copyText[i]
