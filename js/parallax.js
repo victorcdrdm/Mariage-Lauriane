f890b904f30e7d0a7e5c170c2d6344cd9564903d
victorcdrdm <victorcrombez@gmail.com>
2021-06-21 15:57:52 +0200

    fix parralx effect

--- js/parallax.js
+++ js/parallax.js
@@ -4,62 +4,42 @@ let body    = document.getElementById('container-body')
 let annexe  = document.querySelector('.container-annexe')
 let banner  = document.getElementById('banner-welcome')
 let wedding = document.querySelector('.wedding')
-let cards   = document.querySelectorAll('.cards')
+let cards   = document.querySelectorAll('.block-card')
 let picture = document.querySelector('.picture-couple')
 
-let i = 1.0;
+let i = 1;
 
-// observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//         if (entry.intersectionRatio > 0) {
-//             observer.unobserve(entry.target);
-//             action(entry.target)
-//         }
-//     },{threshold: [1]})
-// })
-//
-// cards.forEach(card => {
-//     observer.observe(card)
-// })
-//
+for (i=0; i<cards.length; i++){
+        cards[i].children[0].children[0].style.transform = 'translateX(-100px)'
+        cards[i].children[0].children[1].style.transform = 'translateY(25px)'
+}
 
-//
-// for (i = 0; i < cards.length ; i++) {
-//     let cardTop = cards[i]
-//     cards[i].children[0].style.transform = 'translateX(-150px) translateY(150px)'
-//     cards[i].children[1].style.transform = 'translateY(250px)'
-//     cards[i].children[1].style.transition = 'all 500ms'
-//     action(cardTop, cards[i])
-// }
-//
-//
-// function action(cardTop) {
-//     window.addEventListener('scroll', () => {
-//
-//             if (scrollY +(window.innerHeight - 200) >= cardTop.offsetTop  && scrollY + (window.innerHeight/2-5) <= cardTop.offsetTop ){
-//                 let diff = ((scrollY + window.innerHeight/2+1) - cardTop.offsetTop )
-//                 cardTop.children[0].style.transform = 'translateX(' + (diff /1.87) + 'px) translateY(' + (diff / -1.50) + 'px)'
-//                 cardTop.children[1].style.transform = 'translateY('+ (diff /19) + 'px)'
-//                 cardTop.children[1].style.transition = 'all 2s'
-//             }
-//
-//     })
-// }
-// function action(entry) {
-//     window.addEventListener('scroll', () => {
-//         let diff = ((scrollY + window.innerHeight/2+1) - entry.offsetTop )
-//         let ratio = (diff /133)
-//         if (ratio > 200){
-//             ratio = 200
-//
-//         }
-//         console.log()
-//         console.log(ratio)
-//         entry.style.transform = 'translateX(' + ratio + 'px)'
-//         // cardTop.children[1].style.transform = 'translateY('+ (diff /19) + 'px)'
-//         // cardTop.children[1].style.transition = 'all 2s'
-//     })
-// }
+observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            action(entry.target)
+        }
+    })
+})
+
+cards.forEach(card => {
+    observer.observe(card)
+})
+
+function action(entry) {
+    window.addEventListener('scroll', () => {
+        let diff = ((scrollY + window.innerHeight/2+1) - entry.offsetTop )
+        let ratio = (diff /4)
+        if (ratio > 100){
+            ratio = 100
+        }else if (ratio <= 0 ){
+            ratio = 0
+        }
+        entry.children[0].children[0].style.left =  ratio + 'px'
+        entry.children[0].children[0].style.top =  -ratio + 'px'
+        entry.children[0].children[1].style.top =  -ratio + 'px'
+    })
+}
 
 let boxHeight = picture.offsetHeight * 0.3;
 
